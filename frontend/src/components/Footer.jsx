82454c3a965457db904ae4173af2f56d30d37d71
MAYANK CHANDEL <mayank@webarclight.com>
2025-10-16 19:12:52 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -73,7 +73,7 @@ const Footer = () => {
             <p className='text-lg font-semibold text-gray-800 mb-4'>Follow Us</p>
             <div className='flex gap-4'>
                 <a href="#" className='text-gray-500 hover:text-teal-600 transition-colors duration-300'><FacebookIcon /></a>
-                <a href="#" className='text-gray-500 hover:text-teal-600 transition-colors duration-300'><TwitterIcon /></a>
+                <a href="https://www.instagram.com/cywala_/?igsh=MXIzZmU4Z256OThiOA%3D%3D#" className='text-gray-500 hover:text-teal-600 transition-colors duration-300'><TwitterIcon /></a>
                 <a href="https://www.instagram.com/cywala_/?igsh=MXIzZmU4Z256OThiOA%3D%3D#" className='text-gray-500 hover:text-teal-600 transition-colors duration-300'><InstagramIcon /></a>
             </div>
           </div>
